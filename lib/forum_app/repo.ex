eb3e15ae750842b03828196bec54489599cbb0ca
Mark Szymczyk <mark@meandmark.com>
2024-11-04 15:12:23 -0500

    Update the repo to use AshPostgres instead of Ecto for database storage.

--- lib/forum_app/repo.ex
+++ lib/forum_app/repo.ex
@@ -1,5 +1,8 @@
 defmodule ForumApp.Repo do
-  use Ecto.Repo,
-    otp_app: :forum_app,
-    adapter: Ecto.Adapters.Postgres
+  use AshPostgres.Repo,
+    otp_app: :forum_app
+    
+  def installed_extensions do
+    ["ash-functions", "uuid-ossp", "citext"]
+  end
 end
