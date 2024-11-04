dda7c586eb56f0601a5f59b693174ee6901f24d1
Mark Szymczyk <mark@meandmark.com>
2024-11-04 15:10:03 -0500

    Add Ash dependencies to the formatter.exs file.

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  import_deps: [:ash, :ecto, :ecto_sql, :phoenix],
+  import_deps: [:ash, :ash_phoenix, :ash_postgres, :ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
   plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
