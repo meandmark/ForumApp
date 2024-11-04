e8bda4add1549cb87a5974460bddee2e5bcb0912
Mark Szymczyk <mark@meandmark.com>
2024-11-04 15:40:18 -0500

    Add the leading colon to the :read action.

--- lib/forum_app/forum/forum.ex
+++ lib/forum_app/forum/forum.ex
@@ -4,14 +4,14 @@ defmodule ForumApp.Forum do
   resources do
     resource ForumApp.Forum.Board do
       define :add_board, action: :create
-      define :list_boards, action: read
+      define :list_boards, action: :read
       define :rename_board, action: :update
       define :remove_board, action: :destroy
     end
     
     resource ForumApp.Forum.Post do
       define :add_post, action: :create
-      define :list_posts, action: read
+      define :list_posts, action: :read
       define :update_post, action: :update
       define :remove_post, action: :destroy
     end
