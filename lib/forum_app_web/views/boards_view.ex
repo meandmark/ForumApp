ffa3f2f0502419566e4d2968b63555893c1407f2
Mark Szymczyk <mark@meandmark.com>
2024-11-05 17:38:08 -0500

    Create a list of boards to show.

--- lib/forum_app_web/views/boards_view.ex
+++ lib/forum_app_web/views/boards_view.ex
@@ -7,11 +7,17 @@ defmodule ForumAppWeb.BoardsView do
 	def render(assigns) do
 	  ~H"""
 	  	<h1> Boards </h1>
+	  	
+	  	<ul :for={board <- @boards}>
+	  		<li><%= board.name %></li>
+	  	</ul>
 	  """
 	end
 	
 	def mount(_params, _session, socket) do
 	  boards = Forum.list_boards!()
+	  
+	  socket = assign(socket, boards: boards)
 	  
 	  {:ok, socket}
 	end
